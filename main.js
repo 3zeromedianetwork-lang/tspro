c8af33625d5118fb16d339a3077bd511a43c2280
3zeromedianetwork-lang <3zeromedianetwork@gmail.com>
2026-08-13 01:31:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   console.log('NewsFlash Pro Started');
 
   // --- SERVER-SIDE SHARED STATE ---
@@ -570,9 +570,9 @@
           y = (parseFloat(y) || 0) + event.deltaRect.top;
 
           Object.assign(event.target.style, {
-            width: ${event.rect.width}px,
-            height: ${event.rect.height}px,
-            transform: 	ranslate(px, px)
+            width: `${event.rect.width}px`,
+            height: `${event.rect.height}px`,
+            transform: `translate(${x}px, ${y}px)`
           });
 
           Object.assign(event.target.dataset, { x, y });
